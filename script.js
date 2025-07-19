b06ef03241bd5efaee1c84a9ba36c4ac6e1840c6
ShrijitaRoyChowdhury <shrijitaroychowdhury10@gmail.com>
2025-07-19 20:16:44 +0530

    script.js

--- script.js
+++ script.js
@@ -65,6 +65,14 @@ if (downloadBtn) {
     link.click();
   });
 
+  let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    navbar.classList.toggle('active');
+};
+
+
 
 
 }
